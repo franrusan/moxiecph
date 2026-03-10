db490ffbd7c30da3fd255fa2eefd59ae17fdad37
Fran Rusan <franrusan9@gmail.com>
2026-03-10 09:56:34 +0100

    fix

--- docs/backend/server.js
+++ docs/backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: true } : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 // ─── Config ───────────────────────────────────────────────────────────────────
